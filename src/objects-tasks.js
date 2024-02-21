2a13b39e4ee42d48d4bc1099d429734268141066
pambaka <pambaka@ya.ru>
2024-02-21 22:37:15 +0100

    feat: makeWord is implemented

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -158,8 +158,19 @@ function makeImmutable(/* obj */) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const word = [];
+
+  const letters = Object.keys(lettersObject);
+  const indexes = Object.values(lettersObject);
+
+  for (let i = 0; i < letters.length; i += 1) {
+    indexes[i].forEach((index) => {
+      word[index] = letters[i];
+    });
+  }
+
+  return word.join('');
 }
 
 /**
